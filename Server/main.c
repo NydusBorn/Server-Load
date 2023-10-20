604b941bb5c3bceb5e0dc4fc975fc0bedc75a4e6
Nydus Born <90088743+NydusBorn@users.noreply.github.com>
2023-10-20 22:29:57 +0300

    Better logging, corrected flow control

--- Server/main.c
+++ Server/main.c
@@ -17,67 +17,150 @@
 #include "libpq-fe.h"
 
 static const char HELLO[] = "Welcome to Server Load instance (version 0.1)\nType 'quit' to close the connection\n";
-static const char ERROR_NO_PORT_GIVEN[] = "You have given no port, using the default:45682\n";
+static const char ERROR_NO_PORT_GIVEN[] = "You have given no port\n";
+static const char ERROR_NO_DATABASE_IP_GIVEN[] = "You have given no database_ip\n";
+static const char ERROR_NO_DATABASE_PORT_GIVEN[] = "You have given no database_port\n";
+static const char ERROR_NO_DATABASE_USER_GIVEN[] = "You have given no database_user\n";
+static const char ERROR_NO_DATABASE_PASSWORD_GIVEN[] = "You have given no database_password\n";
+static const char ERRPR_NO_DATABASE_NAME_GIVEN[] = "You have given no database_name\n";
+static const char USAGE[] = "Usage: %s port database_ip database_port database_user database_password database_name\nport - which port to listen on\ndatabase_ip - IP of the server the database is located at\ndatabase_port - port used by the database connections on the server\ndatabase_user - username used to connect to the database\ndatabase_password - password used to connect to the database\ndatabase_name - name of the database\n";
 static int active = 1;
 
 void q_signal_handler(int s) {
     (void) s;
     active = 0;
+    fprintf(stdout, "Closing Server\n");
 }
 
 
 int main(int argc, char **argv) {
     ssize_t HELLO_SIZE = strlen(HELLO);
-    int PORT = 45682;
 
     int sockfd;
     struct sockaddr_in addr;
 
+    setbuf(stdout, NULL);
 
     struct sigaction signal_action;
     signal_action.sa_handler = q_signal_handler;
     sigemptyset(&signal_action.sa_mask);
     signal_action.sa_flags = 0;
     sigaction(SIGINT, &signal_action, NULL);
-
-    if (argc != 2) {
-        fprintf(stdout, "Usage: %s port\n", argv[0]);
-        printf(ERROR_NO_PORT_GIVEN);
+    printf("parsing arguments\n");
+    switch (argc) {
+        case 1:
+            fprintf(stdout, ERROR_NO_PORT_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_IP_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_PORT_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_USER_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_PASSWORD_GIVEN);
+            fprintf(stdout, ERRPR_NO_DATABASE_NAME_GIVEN);
+            fprintf(stdout, USAGE, argv[0]);
+            exit(EXIT_FAILURE);
+        case 2:
+            fprintf(stdout, ERROR_NO_DATABASE_IP_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_PORT_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_USER_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_PASSWORD_GIVEN);
+            fprintf(stdout, ERRPR_NO_DATABASE_NAME_GIVEN);
+            fprintf(stdout, USAGE, argv[0]);
+            exit(EXIT_FAILURE);
+        case 3:
+            fprintf(stdout, ERROR_NO_DATABASE_PORT_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_USER_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_PASSWORD_GIVEN);
+            fprintf(stdout, ERRPR_NO_DATABASE_NAME_GIVEN);
+            fprintf(stdout, USAGE, argv[0]);
+            exit(EXIT_FAILURE);
+        case 4:
+            fprintf(stdout, ERROR_NO_DATABASE_USER_GIVEN);
+            fprintf(stdout, ERROR_NO_DATABASE_PASSWORD_GIVEN);
+            fprintf(stdout, ERRPR_NO_DATABASE_NAME_GIVEN);
+            fprintf(stdout, USAGE, argv[0]);
+            exit(EXIT_FAILURE);
+        case 5:
+            fprintf(stdout, ERROR_NO_DATABASE_PASSWORD_GIVEN);
+            fprintf(stdout, ERRPR_NO_DATABASE_NAME_GIVEN);
+            fprintf(stdout, USAGE, argv[0]);
+            exit(EXIT_FAILURE);
+        case 6:
+            fprintf(stdout, ERRPR_NO_DATABASE_NAME_GIVEN);
+            fprintf(stdout, USAGE, argv[0]);
+            exit(EXIT_FAILURE);
+        case 7: {
+            int args_len = 100;
+            args_len += strlen(argv[2]);
+            args_len += strlen(argv[3]);
+            args_len += strlen(argv[4]);
+            args_len += strlen(argv[5]);
+            args_len += strlen(argv[6]);
+            if (args_len > 4096) {
+                fprintf(stderr, "Too long arguments\n");
+                exit(EXIT_FAILURE);
+            }
+            fprintf(stdout, "Starting Server\n");
+            break;
+        }
+        default:
+            fprintf(stdout, USAGE, argv[0]);
+            exit(EXIT_FAILURE);
     }
 
+    int PORT = atoi(argv[1]);
+    char Conn_info[4096];
+    PGconn *PostGres_conn;
+    PGresult *PostGres_res;
+    {
+        sprintf(Conn_info, "hostaddr=%s port=%s user=%s password=%s dbname=%s", argv[2], argv[3], argv[4], argv[5],
+                argv[6]);
+        fprintf(stdout, "Attempting contact with database server\n");
+        PostGres_conn = PQconnectdb(Conn_info);
+        if (PQstatus(PostGres_conn) != CONNECTION_OK) {
+            fprintf(stderr, "Unable to connect to database: %s\n",
+                    PQerrorMessage(PostGres_conn));
+            PQfinish(PostGres_conn);
+            exit(EXIT_FAILURE);
+        }
+        PQfinish(PostGres_conn);
+        fprintf(stdout, "Connection succesful\n");
+    }
+
+    fprintf(stdout, "Opening port %d\n", PORT);
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    if (argc == 2) {
-        PORT = atoi(argv[1]);
-    }
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
-
+    fprintf(stdout, "Creating socket\n");
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         fprintf(stderr, "Unable to create socket: %s\n",
                 strerror(errno));
         return 1;
     }
+    fprintf(stdout, "Socket created\n");
+    fprintf(stdout, "Binding socket\n");
     if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
         fprintf(stderr, "Unable to bind socket: %s\n",
                 strerror(errno));
         close(sockfd);
         return 1;
     }
+    fprintf(stdout, "Socket bound\n");
+    fprintf(stdout, "Starting to listen for connections\n");
     if (listen(sockfd, 5) < 0) {
         fprintf(stderr, "Unable to listen socket: %s\n",
                 strerror(errno));
         close(sockfd);
         return 1;
     }
-
+    fprintf(stdout, "Port %d opened for connections\n", PORT);
+    fprintf(stdout, "Listing available IP addresses\n");
     {
         char hostbuffer[256];
 
         gethostname(hostbuffer, sizeof(hostbuffer));
 
-        printf("Server running at %s\n", hostbuffer);
-        printf("local ips:\n");
+        fprintf(stdout, "Server running at %s\n", hostbuffer);
+        fprintf(stdout, "local ips:\n");
     }
 
     {
@@ -103,23 +186,24 @@ int main(int argc, char **argv) {
                                 host, NI_MAXHOST,
                                 NULL, 0, NI_NUMERICHOST);
                 if (s != 0) {
-                    printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                    fprintf(stderr, "getnameinfo() failed: %s\n", gai_strerror(s));
                     exit(EXIT_FAILURE);
                 }
 
-                printf("\tAt %s:%s address: %s:%d\n", ifa->ifa_name, "ipv4", host, PORT);
+                fprintf(stdout, "\tAt %s:%s address: %s:%d\n", ifa->ifa_name, "ipv4", host, PORT);
             }
         }
 
         freeifaddrs(ifaddr);
     }
     {
-        FILE  *comm = popen("curl https://api.ipify.org", "r");
+        FILE *comm = popen("curl https://api.ipify.org", "r");
         char buf_comm[1024];
         fgets(buf_comm, 1024, comm);
-        printf("public ip: %s\n", buf_comm);
+        fprintf(stdout, "public ip: %s\n", buf_comm);
         pclose(comm);
     }
+    fprintf(stdout, "Listed available IP addresses\n");
 
     int server_closure = 1;
 
@@ -128,14 +212,23 @@ int main(int argc, char **argv) {
         socklen_t sz = 0;
         int fd;
         pid_t pid;
-
-        if ((fd = accept(sockfd, ca, &sz)) < 0) {
-            fprintf(stderr, "Unable to listen socket: %s\n",
+        fprintf(stdout, "Waiting for connection\n");
+        fd = accept(sockfd, ca, &sz);
+        if (fd < 0) {
+            fprintf(stderr, "Unable to accept connection: %s\n",
                     strerror(errno));
-        } else if ((pid = fork()) < 0) {
+            continue;
+        }
+        fprintf(stdout, "Connection accepted\n");
+        fprintf(stdout, "Forking process\n");
+        pid = fork();
+        if (pid < 0) {
             fprintf(stderr, "Unable to fork process: %s\n",
                     strerror(errno));
-        } else if (pid > 0) {
+            continue;
+        }
+        fprintf(stdout, "Process with pid %d created\n", pid);
+        if (pid > 0) {
             close(fd);
         } else if (send(fd, HELLO, HELLO_SIZE, 0) == HELLO_SIZE) {
             int f = 1;
@@ -146,22 +239,28 @@ int main(int argc, char **argv) {
                 struct tm tm = *localtime(&t);
                 struct sockaddr_in client_addr;
                 socklen_t addr_size = sizeof(struct sockaddr_in);
-                getpeername(fd, (struct sockaddr *)&client_addr, &addr_size);
+                getpeername(fd, (struct sockaddr *) &client_addr, &addr_size);
                 strcpy(clientip, inet_ntoa(client_addr.sin_addr));
-                printf("[%s entered at %d-%02d-%02d %02d:%02d:%02d]\n", clientip, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+                fprintf(stdout, "[%s entered at %d-%02d-%02d %02d:%02d:%02d]\n", clientip, tm.tm_year + 1900,
+                        tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
             }
             while (f) {
                 memset(buf, 0, sizeof(buf));
-                if ((recv(fd, buf, 254, 0)) > 0) {
+                if (recv(fd, buf, 254, 0) > 0) {
                     time_t t = time(NULL);
                     struct tm tm = *localtime(&t);
-                    printf("[from %s at %d-%02d-%02d %02d:%02d:%02d] %s\n", clientip, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, buf);
+                    fprintf(stdout, "[from %s at %d-%02d-%02d %02d:%02d:%02d] %s\n", clientip, tm.tm_year + 1900,
+                            tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, buf);
                     if (strncmp(buf, "quit", 4) == 0) {
                         f = 0;
                     }
+                } else {
+                    f = 0;
                 }
             }
-
+            
+            fprintf(stdout, "Closing connection from %s\n", clientip);
+            setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, NULL, sizeof(int));
             shutdown(fd, SHUT_RDWR);
             close(fd);
             active = 0;
@@ -176,12 +275,12 @@ int main(int argc, char **argv) {
 
 
     if (server_closure) {
-        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,  NULL, sizeof(int));
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, NULL, sizeof(int));
         shutdown(sockfd, SHUT_RDWR);
         close(sockfd);
 
-        printf("Closed Server appropriately\n");
+        fprintf(stdout, "Closed Server appropriately\n");
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
